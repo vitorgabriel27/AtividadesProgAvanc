c1b23cfa8fecddfc69121cf2817624989e3c8553
Vitor Gabriel <vitorg135@gmail.com>
2021-07-22 12:32:17 -0300

    off Examples

--- ProjetoEscultor/runProject/main.cpp
+++ ProjetoEscultor/runProject/main.cpp
@@ -5,7 +5,7 @@ using namespace std;
 
 int main(){
 
-    Sculptor sword(20, 20, 20);
+    /*Sculptor sword(20, 20, 20);
 
     //ponta de baixo
     sword.setColor(0.03,0.14,0.12,1.0);//azul marinho
@@ -82,6 +82,127 @@ int main(){
 
     sword.writeOFF("sword.off");
 
-    return 0;
+    */
 
+    /*Sculptor flower(25,25,25);
+
+    //base
+    flower.setColor(0.39,0.66,0.22,1.0);
+    flower.putBox(2,7,1,8,0,1);
+
+    flower.putBox(1,2,1,8,1,2);
+    flower.putBox(7,8,1,8,1,2);
+
+    flower.putBox(0,1,1,8,2,7);
+    flower.putBox(8,9,1,8,2,7);
+
+    flower.putBox(1,2,1,8,7,8);
+    flower.putBox(7,8,1,8,7,8);
+
+    flower.putBox(2,7,1,8,8,9);
+
+    flower.putEllipsoid(4,0,4,4,1,4);
+
+    //firula
+    flower.setColor(0.37,0.27,0.13,1.0);
+    flower.putEllipsoid(4,7,4,4,1,4);
+
+    //tronco
+    flower.setColor(0.39,0.66,0.22,1.0);
+    flower.putEllipsoid(4,8,4,2,8,2);
+
+    flower.cutBox(3,4,8,16,3,4);
+    flower.cutBox(3,4,8,16,5,6);
+    flower.cutBox(5,6,8,16,3,4);
+    flower.cutBox(5,6,8,16,5,6);
+
+    //left-leaf
+    flower.putBox(2,3,8,9,3,6);
+    flower.putBox(1,2,9,10,3,6);
+    flower.putBox(0,1,9,10,4,5);
+    //right-leaf
+    flower.putBox(6,7,10,11,3,6);
+    flower.putBox(7,9,11,12,3,6);
+    flower.putBox(9,10,11,12,4,5);
+
+    //head
+    flower.setColor(1.0,0.22,0.1,1.0);
+    flower.putSphere(4,19,4,5);
+
+    //mais firula
+    flower.setColor(1.0,1.0,1.0,1.0);
+    flower.putBox(2,7,21,22,9,10);
+    flower.putBox(2,7,17,18,9,10);
+
+    flower.putVoxel(1,20,8);
+    flower.putVoxel(0,19,7);
+    flower.putVoxel(7,20,8);
+    flower.putVoxel(8,19,7);
+
+    flower.putVoxel(7,18,8);
+    flower.putVoxel(1,18,8);
+
+    flower.putVoxel(2,18,9);
+    flower.putVoxel(3,20,9);
+    flower.putVoxel(4,18,9);
+    flower.putVoxel(5,20,9);
+    flower.putVoxel(6,18,9);
+
+
+
+    flower.writeOFF("flower.off");*/
+
+    Sculptor astronauta(25,25,25);
+
+    //legs
+    astronauta.setColor(0.19,0.19,0.19,1.0);
+    astronauta.putBox(3,6,0,1,10,16);
+    astronauta.putBox(8,11,0,1,10,16);
+
+    astronauta.setColor(1.0,1.0,1.0,1.0);
+    astronauta.putBox(3,6,1,5,10,13);
+    astronauta.putBox(8,11,1,5,10,13);
+
+    //tronco
+    astronauta.putBox(3,11,5,14,7,16);
+
+    //braços
+    astronauta.putBox(0,3,7,10,9,16);
+    astronauta.putBox(11,14,7,10,9,16);
+
+    astronauta.putBox(0,3,10,12,9,12);
+    astronauta.putBox(11,14,10,12,9,12);
+
+    astronauta.setColor(0.19,0.19,0.19,1.0);
+    astronauta.putBox(0,3,7,10,16,17);
+    astronauta.putBox(11,14,7,10,16,17);
+
+    //head
+    astronauta.setColor(1.0,1.0,1.0,1.0);
+    astronauta.putBox(1,13,14,15,6,17);
+    astronauta.putBox(1,13,14,22,6,8);
+    astronauta.putBox(1,13,22,23,6,17);
+
+    astronauta.setColor(0.0,0.55,0.8,1.0);
+    astronauta.putBox(1,13,15,22,8,9);
+
+    astronauta.setColor(0.19,0.19,0.19,1.0);
+    astronauta.putBox(1,13,15,22,9,17);
+
+    //oxygen
+    astronauta.setColor(0.65,0.65,0.65,1.0);
+    astronauta.putBox(3,11,5,14,2,7);
+    astronauta.cutBox(3,11,5,6,6,7);
+    astronauta.cutBox(3,11,13,14,6,7);
+    astronauta.putBox(5,6,14,18,3,4);
+    astronauta.putBox(7,8,14,24,3,4);
+
+    astronauta.setColor(0.3,0.3,0.3,1.0);
+    astronauta.putVoxel(5,18,3);
+    astronauta.putVoxel(7,24,3);
+
+
+    astronauta.writeOFF("astronauta.off");
+
+    return 0;
 }
